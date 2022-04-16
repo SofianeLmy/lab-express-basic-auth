11e57309e7949856f3baf32550069e7c16894188
Sofiane M <poubellepropre8@gmail.com>
2022-04-16 16:58:15 +0200

    updates

--- routes/base.js
+++ routes/base.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = new express.Router();
+const User = require('./../models/user');
+const bcryptjs = require('bcryptjs');
 
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');
 });
 
 module.exports = router;
